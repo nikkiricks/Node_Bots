186a9f4f594b3a6a8fa15e5e744c0153e56d09f9
nikkiricks <nikki.ricks@gmail.com>
2020-02-12 10:15:35 +1100

    updating email-button file to rework for new arduino board

--- email-button.js
+++ email-button.js
@@ -5,49 +5,53 @@ var board = new arduino.Board()
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+var userDetails = {
+  email: 'nikki.ricks@gmail.com',
+  phoneNumber: '61488611347@e2s.messagemedia.com'
+}
+
+
 board.on("ready", function() {
   var buttonTwo = new arduino.Button(2); // 🙂 on pin 2
   var buttonFour = new arduino.Button(4) // 😢 on pin 4
   var buttonSix = new arduino.Button(6) // 🦄 on pin 6
 
-  buttonTwo.on("up", function() {
-    const msg = {
-      to: '61488611347@e2s.messagemedia.com',
-      // to: 'hey@feminest.co',
-      from: 'nikki.ricks@gmail.com',
-      subject: '🙂',
-      text: 'I am having a great time, no need to come home!🙂',
-      html: '🙂I am having a great time, no need to come home!🙂',
-    };
-    sgMail.send(msg);
-    console.log(msg)
-  });
+  // buttonTwo.on("up", function() {
+  //   const msg = {
+  //     to: '61488611347@e2s.messagemedia.com',
+  //     // to: 'hey@feminest.co',
+  //     from: 'nikki.ricks@gmail.com',
+  //     subject: '🙂',
+  //     text: 'I am having a great time, no need to come home!🙂',
+  //     html: '🙂I am having a great time, no need to come home!🙂',
+  //   };
+  //   sgMail.send(msg);
+  //   console.log(msg)
+  // });
 
-  buttonFour.on("up", function() {
-    const msg = {
-      to: '61488611347@e2s.messagemedia.com',
-      // to: 'hey@feminest.co',
-      from: 'nikki.ricks@gmail.com',
-      subject: '😢',
-      text: 'Please come home!😢',
-      html: '😢 Please come home!😢',
-    };
-    sgMail.send(msg);
-    console.log(msg)
-  });
+  // buttonFour.on("up", function() {
+  //   const msg = {
+  //     to: '61488611347@e2s.messagemedia.com',
+  //     // to: 'hey@feminest.co',
+  //     from: 'nikki.ricks@gmail.com',
+  //     subject: '😢',
+  //     text: 'Please come home!😢',
+  //     html: '😢 Please come home!😢',
+  //   };
+  //   sgMail.send(msg);
+  //   console.log(msg)
+  // });
 
   buttonSix.on("up", function() {
     const msg = {
       to: '61488611347@e2s.messagemedia.com',
-      // to: 'hey@feminest.co',
+     // to: userDetails.phoneNumber,
       from: 'nikki.ricks@gmail.com',
-      subject: '',
-      text: `Hi! It was so lovely meeting you tonight. <br> My name is Nikki Ricks and I am looking for a software engineering role. This is not my phone number (I used an API), if you want to call or text, please contact me at 0488 611 347. <br> Here are some other ways I'm more than happy to connect: <br> EMAIL: nikki.ricks@gmail.com <br>  LINKEDIN: https://www.linkedin.com/in/nikki-ricks/ <br> GITHUB: https://github.com/nikkiricks <br> TWITTER: https://twitter.com/nikkiricks`,
-      html: '',
+      subject: '🙋‍♀️ Hi This is Nikki from the Meet and Greet!',
+      text: 'This is not my phone number (I used an API), if you want to call or text: 0488 611 347. And some other ways to connect! EMAIL: nikki.ricks@gmail.com LINKEDIN: https://www.linkedin.com/in/nikki-ricks/ GITHUB: https://github.com/nikkiricks TWITTER: https://twitter.com/nikkiricks',
+      html: 'This is not my phone number (I used an API), if you want to call or text: 0488 611 347. And some other ways to connect! EMAIL: nikki.ricks@gmail.com LINKEDIN: https://www.linkedin.com/in/nikki-ricks/ GITHUB: https://github.com/nikkiricks TWITTER: https://twitter.com/nikkiricks',
     };
     sgMail.send(msg);
     console.log(msg)
   });
-
-
 });
