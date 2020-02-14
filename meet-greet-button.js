892f17517daf89ffd4e399cfa11796efb5f5d33d
nikkiricks <nikki.ricks@gmail.com>
2020-02-14 15:04:25 +1100

    updated meet and greet file for multiple input types

--- meet-greet-button.js
+++ meet-greet-button.js
@@ -6,15 +6,22 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 board.on("ready", function() {
-  var buttonSix = new arduino.Button(6) // 🦄 on pin 6
+  var button = new arduino.Button(6) 
 
-  buttonSix.on("up", function() {
+  button.on("up", function() {
     const msg = {
-      to: '61488611347@e2s.messagemedia.com',
+      // text only
+      // to: '61403369786@e2s.messagemedia.com',
+
+      // email only
+      // to: ['sam.gilding@tabcorp.com.au', 'nikki.ricks@gmail.com'],
+
+      // both text and email
+      to: ['jonathan-samuel@hotmail.com', '61400860869@e2s.messagemedia.com', 'nikki.ricks@gmail.com'],
+
       from: 'nikki.ricks@gmail.com',
-      subject: '🙋‍♀️ Hi This is Nikki from the Meet and Greet!',
-      text: 'This is not my phone number (I used an API), if you want to call or text: 0488 611 347. And some other ways to connect! EMAIL: nikki.ricks@gmail.com LINKEDIN: https://www.linkedin.com/in/nikki-ricks/ GITHUB: https://github.com/nikkiricks TWITTER: https://twitter.com/nikkiricks',
-      html: 'This is not my phone number (I used an API), if you want to call or text: 0488 611 347. And some other ways to connect! EMAIL: nikki.ricks@gmail.com LINKEDIN: https://www.linkedin.com/in/nikki-ricks/ GITHUB: https://github.com/nikkiricks TWITTER: https://twitter.com/nikkiricks',
+      subject: '🙋‍♀️ Hi! Nikki from the Meet and Greet.',
+      html: "Let's connect! 📞: 0488 611 347 📧: nikki.ricks@gmail.com 🔗: https://www.linkedin.com/in/nikki-ricks/ 🤓: https://github.com/nikkiricks",
     };
     sgMail.send(msg);
     console.log(msg)
